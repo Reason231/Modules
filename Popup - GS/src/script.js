3f7ef402957afa3776c531647efa343da7f3b942
Reason231 <rijankhadgi100@gmail.com>
2024-10-16 10:17:19 +0545

    16th October

--- Popup - GS/src/script.js
+++
@@ -1,8 +0,0 @@
-const container=document.getElementById("container")
-const submitbtn=document.getElementById("submitbtn")
-
-submitbtn.addEventListener("click",(e)=>{
-    container.style.visibility="visible"
-    container.style.transform = "translateY(0px)";
-    submitbtn.style.visibility="hidden"
-})
